f01ee2fe8c56cf9426c6a8576036677b35755e2d
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-04-20 21:31:24 +0200

    finding group members through neo4j.

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,6 +10,15 @@ class Group < ActiveRecord::Base
   end
   
   
+  def <<(object)
+    neo_node.children << object.neo_node
+  end
+  
+  def members
+    User.find neo_node.member_ids
+  end
+  
+  
   def title
     self.name
   end
--- app/models/neo_node.rb
+++ app/models/neo_node.rb
@@ -5,6 +5,20 @@ class NeoNode
   property :active_record_id, type: Integer, index: :exact
   property :title
   
+  has_many :out, :children, model_class: NeoNode
+  has_many :in, :parents, model_class: NeoNode
+  
+  
+  def member_ids
+    # TODO: Check for a better syntax here: http://stackoverflow.com/questions/29755869
+    self.query_as(:self).match("path = (member:User)<-[*]-self")
+      .where("all(node in tail(nodes(path)) where ('Group' in labels(node)))")
+      .pluck('member.active_record_id')
+  end
+  
+  
+  
+  
   # Example:
   #   neo_node.sync_from(user)
   #
@@ -12,9 +26,16 @@ class NeoNode
     self.active_record_type = active_record_object.class.name
     self.active_record_id = active_record_object.id || raise('active_record_object must have an id')
     self.title = active_record_object.title if active_record_object.respond_to?(:title)
+    self.add_label self.active_record_type
     self.save
   end
   
+
+  # Returns the corresponding ActiveRecord object.
+  #
+  def to_active_record
+    active_record_object
+  end
   def active_record_object
     active_record_class.find self.active_record_id
   end
